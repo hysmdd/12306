372447fd4bd842410672c455591c075fc8665283
Martis <1801882100@qq.com>
2023-08-08 21:17:17 +0800

    增加login路由及页面

---
+++ web/src/views/login.vue
@@ -0,0 +1,58 @@
+<template>
+  <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+        label="手机号"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+    >
+      <a-input v-model:value="formState.username" />
+    </a-form-item>
+
+    <a-form-item
+        label="密码"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+    >
+      <a-input-password v-model:value="formState.password" />
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  setup() {
+    const formState = reactive({
+      username: '',
+      password: '',
+    });
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+    return {
+      formState,
+      onFinish,
+      onFinishFailed,
+    };
+  },
+});
+</script>
+
+<style>
+
+</style>
