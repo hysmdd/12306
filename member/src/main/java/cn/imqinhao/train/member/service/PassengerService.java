2ac5a7489bb12a794b5fb3f94e04bc6f55699d2a
Martis <1801882100@qq.com>
2023-08-09 20:14:18 +0800

    乘车人编辑接口开发

--- member/src/main/java/cn/imqinhao/train/member/service/PassengerService.java
+++ member/src/main/java/cn/imqinhao/train/member/service/PassengerService.java
@@ -40,13 +40,20 @@ public class PassengerService {
      * @param req 乘车人信息
      */
     public void save(PassengerSaveReq req) {
-        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setMemberId(LoginMemberContext.getId());
         DateTime now = DateTime.now();
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
-        passengerMapper.insert(passenger);
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        // id为空，表示保存
+        if (ObjectUtil.isNull(passenger.getId())) {
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            // id不为空，表示修改
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
     }
 
     /**
@@ -57,23 +64,37 @@ public class PassengerService {
      * @return 乘车人
      */
     public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
+        // 获取会员ID
         Long memberId = req.getMemberId();
+        // 构造条件
         PassengerExample passengerExample = new PassengerExample();
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        // 如果会员ID不为空
         if (ObjectUtil.isNotNull(memberId)) {
+            // 添加条件
             criteria.andMemberIdEqualTo(memberId);
         }
+        // 打印日志
         LOG.info("查询页码：{}", req.getPage());
         LOG.info("每页条数：{}", req.getSize());
+        // 分页查询
         PageHelper.startPage(req.getPage(), req.getSize());
+        // 根据条件查询会员列表
         List<Passenger> passengers = passengerMapper.selectByExample(passengerExample);
+        // 构造分页对象
         PageInfo<Passenger> pageInfo = new PageInfo<>(passengers);
+        // 打印日志
         LOG.info("总行数：{}", pageInfo.getTotal());
         LOG.info("总页数：{}", pageInfo.getPages());
+        // 拷贝对象
         List<PassengerQueryResp> list = BeanUtil.copyToList(passengers, PassengerQueryResp.class);
+        // 构造返回数据
         PageResp<PassengerQueryResp> objectPageResp = new PageResp<>();
+        // 设置匹配的会员总数量
         objectPageResp.setTotal(pageInfo.getTotal());
+        // 封装数据
         objectPageResp.setList(list);
+        // 返回数据
         return objectPageResp;
     }
 
