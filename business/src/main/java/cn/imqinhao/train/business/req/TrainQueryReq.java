71eae18365cec1a9a6ddc09cc6f5b4a58773ef7f
Martis <1801882100@qq.com>
2023-08-11 09:50:57 +0800

    test

--- business/src/main/java/cn/imqinhao/train/business/req/TrainQueryReq.java
+++ business/src/main/java/cn/imqinhao/train/business/req/TrainQueryReq.java
@@ -3,7 +3,6 @@ package cn.imqinhao.train.business.req;
 import cn.imqinhao.train.common.req.PageReq;
 
 public class TrainQueryReq extends PageReq {
-
     @Override
     public String toString() {
         return "TrainQueryReq{" +
