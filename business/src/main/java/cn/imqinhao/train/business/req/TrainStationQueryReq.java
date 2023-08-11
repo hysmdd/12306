93316e9c8019c15ade950d94e12198c52de28fd2
Martis <1801882100@qq.com>
2023-08-11 08:54:20 +0800

    火车车站表，生成持久层代码及前后端代码

---
+++ business/src/main/java/cn/imqinhao/train/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package cn.imqinhao.train.business.req;
+
+import cn.imqinhao.train.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
