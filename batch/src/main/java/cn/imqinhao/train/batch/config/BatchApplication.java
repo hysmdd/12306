609e119255bc75cbb3234cb83668e6c686a7c842
Martis <1801882100@qq.com>
2023-08-12 11:35:28 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/cn/imqinhao/train/batch/config/BatchApplication.java
@@ -0,0 +1,29 @@
+package cn.imqinhao.train.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author Martis
+ * @create 2023-08-12 11:26:13
+ */
+@SpringBootApplication
+@ComponentScan("cn.imqinhao")
+@MapperScan("cn.imqinhao.train.*.mapper")
+public class BatchApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址：http://127.0.0.1:{}{}", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
+    }
+
+}
---
+++ batch/src/main/java/cn/imqinhao/train/batch/controller/TestController.java
@@ -0,0 +1,18 @@
+package cn.imqinhao.train.batch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Martis
+ * @create 2023-08-07 19:17:13
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
