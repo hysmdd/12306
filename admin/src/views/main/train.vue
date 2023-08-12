0783e072640884b6f42b9f1a92081fa4520f7a92
Martis <1801882100@qq.com>
2023-08-12 09:04:27 +0800

    实现按车次生成车座功能，增加前端功能，成功

--- admin/src/views/main/train.vue
+++ admin/src/views/main/train.vue
@@ -20,6 +20,13 @@
             <a style="color: red">删除</a>
           </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
+          <a-popconfirm
+            title="生成座位将删除已有记录，确认生成座位？"
+            @confirm="genSeat(record)"
+            ok-text="确认"
+            cancel-text="取消">
+            <a>生成座位</a>
+          </a-popconfirm>
         </a-space>
       </template>
       <template v-else-if="column.dataIndex === 'type'">
@@ -239,6 +246,19 @@ export default defineComponent({
       });
     };
 
+    const genSeat = (record) => {
+      loading.value = true;
+      axios.get("/business/admin/train/gen-seat/" + record.code).then(res => {
+        loading.value = false;
+        const data = res.data;
+        if(data.success) {
+          notification.success({description: "生成成功！"})
+        } else {
+          notification.success({description: data.message})
+        }
+      })
+    }
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -259,7 +279,8 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      genSeat
     };
   },
 });
