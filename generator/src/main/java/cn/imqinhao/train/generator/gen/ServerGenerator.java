4a6c86ed1691a3b1143d37245905a73ba02ddf8f
Martis <1801882100@qq.com>
2023-08-12 09:50:20 +0800

    重新生成火车座位页面，只读

--- generator/src/main/java/cn/imqinhao/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/cn/imqinhao/train/generator/gen/ServerGenerator.java
@@ -19,7 +19,7 @@ import java.util.*;
  */
 public class ServerGenerator {
 
-    static boolean readOnly = false;
+    static boolean readOnly = true;
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/cn/imqinhao/train/[module]/";
     static String pomPath = "generator/pom.xml";
@@ -84,11 +84,11 @@ public class ServerGenerator {
         param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
 
-        generate(Domain, param, "service", "service");
-        generate(Domain, param, "controller/admin", "adminController");
-        generate(Domain, param, "req", "saveReq");
-        generate(Domain, param, "req", "queryReq");
-        generate(Domain, param, "resp", "queryResp");
+        // generate(Domain, param, "service", "service");
+        // generate(Domain, param, "controller/admin", "adminController");
+        // generate(Domain, param, "req", "saveReq");
+        // generate(Domain, param, "req", "queryReq");
+        // generate(Domain, param, "resp", "queryResp");
         generateVue(do_main, param);
     }
 
