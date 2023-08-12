240807e8e6644ab52073b708b463285e23997769
Martis <1801882100@qq.com>
2023-08-12 21:12:09 +0800

    增加生产每日车次定时任务

---
+++ batch/src/main/java/cn/imqinhao/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,28 @@
+package cn.imqinhao.train.batch.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+/**
+ * @author Martis
+ * @create 2023-08-12 21:06:30
+ */
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+    }
+}
