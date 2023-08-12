9896d8e07d0badb470d2126569cb6202aa90047e
Martis <1801882100@qq.com>
2023-08-12 16:09:18 +0800

    为每日车次页面增加日期和车次查询条件

--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -1,7 +1,9 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" style="width: 200px;" />
+      <train-select-view v-model="params.code" width="300px"></train-select-view>
+      <a-button type="primary" @click="handleQuery()">搜索</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
   </p>
@@ -103,6 +105,10 @@ export default defineComponent({
       pageSize: 10,
     });
     let loading = ref(false);
+    let params = ref({
+      code: null,
+      date: null
+    })
     const columns = [
     {
       title: '日期',
@@ -188,7 +194,7 @@ export default defineComponent({
           visible.value = false;
           handleQuery({
             page: pagination.value.current,
-            size: pagination.value.pageSize
+            size: pagination.value.pageSize,
           });
         } else {
           notification.error({description: data.message});
@@ -207,7 +213,9 @@ export default defineComponent({
       axios.get("/business/admin/daily-train/query-list", {
         params: {
           page: param.page,
-          size: param.size
+          size: param.size,
+          code: params.value.code,
+          date: params.value.date
         }
       }).then((response) => {
         loading.value = false;
@@ -261,7 +269,8 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
-      onChangeCode
+      onChangeCode,
+      params
     };
   },
 });
