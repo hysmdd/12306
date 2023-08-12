7e63860940b097cfd53b09156379f5ac83c7c92b
Martis <1801882100@qq.com>
2023-08-12 12:24:04 +0800

    禁止任务并发执行

--- batch/src/main/java/cn/imqinhao/train/batch/job/TestJob.java
+++ batch/src/main/java/cn/imqinhao/train/batch/job/TestJob.java
@@ -1,5 +1,6 @@
 package cn.imqinhao.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,10 +9,17 @@ import org.quartz.JobExecutionException;
  * @author Martis
  * @create 2023-08-12 11:53:58
  */
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob TEST开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob TEST结束");
     }
 }
