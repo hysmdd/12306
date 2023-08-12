6e04507fed69f5b5072c40d6879bc9dbb3e202f1
Martis <1801882100@qq.com>
2023-08-12 19:44:57 +0800

    增加每日座位表，生成前后端代码

---
+++ business/src/main/java/cn/imqinhao/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package cn.imqinhao.train.business.req;
+
+import cn.imqinhao.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
