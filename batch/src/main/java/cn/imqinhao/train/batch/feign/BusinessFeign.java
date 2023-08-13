0abae7fc0187798e04e5ccb1f9219aa06324e158
Martis <1801882100@qq.com>
2023-08-13 10:34:18 +0800

    增加生成每日车次功能

--- batch/src/main/java/cn/imqinhao/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/cn/imqinhao/train/batch/feign/BusinessFeign.java
@@ -1,7 +1,12 @@
 package cn.imqinhao.train.batch.feign;
 
+import cn.imqinhao.train.common.resp.CommonResp;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
 
 /**
  * @author Martis
@@ -12,5 +17,8 @@ public interface BusinessFeign {
 
     @GetMapping("/hello")
     String hello();
+
+    @GetMapping("/admin/daily-train/gen-daily/{date}")
+    CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 
 }
