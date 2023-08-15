d9f7aafd254d3377093c54d515ac53ce4fdd5f0e
Martis <1801882100@qq.com>
2023-08-15 13:10:24 +0800

    扣减余票数量，并判断余票是否足够

--- common/src/main/java/cn/imqinhao/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/cn/imqinhao/train/common/exception/BusinessExceptionEnum.java
@@ -6,11 +6,14 @@ public enum BusinessExceptionEnum {
     MEMBER_MOBILE_NOT_EXIST("请先获取短信验证码"),
     MEMBER_MOBILE_CODE_ERROR("短信验证码错误"),
     MEMBER_SIZE_TOO_BIG("最多只能添加50名乘客"),
+
     BUSINESS_STATION_NAME_UNIQUE_ERROR("车站已存在"),
     BUSINESS_TRAIN_CODE_UNIQUE_ERROR("车次编号已存在"),
     BUSINESS_TRAIN_STATION_INDEX_UNIQUE_ERROR("同车次站序已存在"),
     BUSINESS_TRAIN_STATION_NAME_UNIQUE_ERROR("同车次站名已存在"),
-    BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR("同车次厢号已存在");
+    BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR("同车次厢号已存在"),
+
+    CONFIRM_ORDER_TICKET_COUNT_ERROR("余票不足");
 
     private String desc;
 
